fdb3472ccc4e797eb6a3aba9f25dc900343d381d
Sven Kapudija <sven.kapudija@gmail.com>
2012-04-13 20:20:42 +0200

    Removed numbers from Demo printout

--- javadz4/src/hr/fer/zemris/java/custom/scripting/demo/ObjectMultistackDemo.java
+++ javadz4/src/hr/fer/zemris/java/custom/scripting/demo/ObjectMultistackDemo.java
@@ -11,20 +11,20 @@ public class ObjectMultistackDemo {
 		multistack.push("year", year);
 		ValueWrapper price = new ValueWrapper(200.51);
 		multistack.push("price", price);
-		System.out.println("1Current value for year: " + multistack.peek("year").getValue());
-		System.out.println("2Current value for price: " + multistack.peek("price").getValue());
+		System.out.println("Current value for year: " + multistack.peek("year").getValue());
+		System.out.println("Current value for price: " + multistack.peek("price").getValue());
 		multistack.push("year", new ValueWrapper(Integer.valueOf(1900)));
-		System.out.println("3Current value for year: " + multistack.peek("year").getValue());
+		System.out.println("Current value for year: " + multistack.peek("year").getValue());
 		multistack.peek("year").setValue(
 				((Integer) multistack.peek("year").getValue()).intValue() + 50);
-		System.out.println("4Current value for year: " + multistack.peek("year").getValue());
+		System.out.println("Current value for year: " + multistack.peek("year").getValue());
 		multistack.pop("year");
-		System.out.println("5Current value for year: " + multistack.peek("year").getValue());
+		System.out.println("Current value for year: " + multistack.peek("year").getValue());
 		multistack.peek("year").increment("5");
-		System.out.println("6Current value for year: " + multistack.peek("year").getValue());
+		System.out.println("Current value for year: " + multistack.peek("year").getValue());
 		multistack.peek("year").increment(5);
-		System.out.println("7Current value for year: " + multistack.peek("year").getValue());
+		System.out.println("Current value for year: " + multistack.peek("year").getValue());
 		multistack.peek("year").increment(5.0);
-		System.out.println("8Current value for year: " + multistack.peek("year").getValue());
+		System.out.println("Current value for year: " + multistack.peek("year").getValue());
 	}
 }
