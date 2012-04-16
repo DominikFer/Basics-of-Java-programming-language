ab7f32b2cd3cf050e109224e9d3f680e3f3fecbd
Sven Kapudija <sven.kapudija@gmail.com>
2012-04-16 02:32:52 +0200

    Added encrpyt/decrypt options

--- javadz5/src/hr/fer/zemris/java/tecaj/hw5/crypto/Crypto.java
+++ javadz5/src/hr/fer/zemris/java/tecaj/hw5/crypto/Crypto.java
@@ -16,35 +16,35 @@ public class Crypto {
 		} else if(args[0].equals("encrypt") || args[0].equals("decrypt")) {
 			if(args.length != 3) throw new IllegalArgumentException();
 			
-			fileEncryption(args[1], args[2]);
+			fileEncryption(args[1], args[2], args[0]);
 		} else {
 			throw new IllegalArgumentException();
 		}
 	}
 	
-	private static void fileEncryption(String inputFileName, String outputFileName) {
+	private static void fileEncryption(String inputFileName, String outputFileName, String mode) {
 		String encryptionKey = "";
 		String initializationVector = "";
 		
 		Scanner sc = new Scanner(System.in);
 		
 		System.out.println("Please provide password as hex-encoded text:");
-		encryptionKey = sc.nextLine().trim();
-		//encryptionKey = "a52217e3ee213ef1ffdee3a192e2ac7e";
+		//encryptionKey = sc.nextLine().trim();
+		encryptionKey = "a52217e3ee213ef1ffdee3a192e2ac7e";
 		
 		System.out.println("Please provide initialization vector as hex-encoded text:");
-		initializationVector = sc.nextLine().trim();
-		//initializationVector = "000102030405060708090a0b0c0d0e0f";
+		//initializationVector = sc.nextLine().trim();
+		initializationVector = "000102030405060708090a0b0c0d0e0f";
 		
 		FileEncryption encryption = new FileEncryption(
 				inputFileName,
 				outputFileName,
 				encryptionKey,
 				initializationVector,
-				FileEncryption.DECRYPT_MODE
+				mode.equals("encrypt") ? FileEncryption.ENCRYPT_MODE : FileEncryption.DECRYPT_MODE
 		);
 		if(encryption.process()) {
-			System.out.println("Decryption completed. Generated file " + inputFileName + " based on file " + outputFileName + ".");
+			System.out.println(mode.equals("encrypt") ? "Encryption" : "Decryption" + " completed. Generated file " + inputFileName + " based on file " + outputFileName + ".");
 		} else {
 			System.out.println("Error.");
 		}
