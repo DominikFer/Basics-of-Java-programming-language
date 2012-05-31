501486d6afa6c5f8c217a66344f2d1781159204f
Sven Kapudija <sven.kapudija@gmail.com>
2012-05-31 18:19:24 +0200

    Added 12. predavanje

---
+++ predavanja-source/predavanje12/src/hr/fer/zemris/java/tecaj_12/servleti/DrugiServlet.java
@@ -0,0 +1,23 @@
+package hr.fer.zemris.java.tecaj_12.servleti;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet(name="S2",urlPatterns={"/dr","/drugi"})
+public class DrugiServlet extends HttpServlet {
+
+	private static final long serialVersionUID = 1L;
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		resp.setContentType("text/html");
+		resp.setCharacterEncoding("UTF-8");
+		resp.getWriter().print("<h1>Hello, World, drugi pokušaj!</h1>");
+	}
+	
+}
